0298fbb03451e4c043fffbc1b5a6a5ce92bcc7a3
Földi Tamás <tfoldi@xsi.hu>
2019-12-08 17:28:36 -0500

    initial release

---
+++ cockroachdb_odbc/connectionBuilder.js
@@ -0,0 +1,31 @@
+(function dsbuilder(attr)
+{
+    var params = {};
+
+    params["SERVER"] = attr[connectionHelper.attributeServer];
+    params["PORT"] = attr[connectionHelper.attributePort];
+    params["DATABASE"] = attr[connectionHelper.attributeDatabase];
+    params["UID"] = attr[connectionHelper.attributeUsername];
+    params["PWD"] = attr[connectionHelper.attributePassword];
+    params["BOOLSASCHAR"] = "0";
+    params["LFCONVERSION"] = "0";
+    params["UseDeclareFetch"] = "0";
+    params["Fetch"] = "2048";
+
+    if (attr[connectionHelper.attributeSSLMode] == "require")
+    {
+        params["sslmode"] = "require";
+    }
+
+
+    var formattedParams = [];
+
+    formattedParams.push(connectionHelper.formatKeyValuePair(driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
+
+    for (var key in params)
+    {
+        formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
+    }
+
+    return formattedParams;
+})
